0368674c466235e6568efbe5e9a8e33eb6291f8b
Grigory Solovyov <gs@dvhb.ru>
2020-09-03 16:45:49 +0300

    fix: moved price counting restriction into variable

--- src/utils/prices.js
+++ src/utils/prices.js
@@ -4,7 +4,9 @@ import moment from "moment";
 import type { RentData } from "../store/types/cart";
 
 export const getBookPrice = (book: ?Book, rentData: ?RentData): number => {
-  if (!book || !rentData || !rentData.from || !rentData.until) return 0;
+  const hasNoBookPrice =
+    !book || !rentData || !rentData.from || !rentData.until;
+  if (hasNoBookPrice) return 0;
   const untilMoment = moment(rentData.until);
   if (untilMoment.isBefore(rentData.from)) return 0;
   return untilMoment.diff(rentData.from, "day");
